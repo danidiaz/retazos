d9199ce36b810c0073c5b78843c7ad4f82bcaac3
Daniel Diaz <diaz.carrete@facebook.com>
2021-02-16 21:45:41 +0100

    elemIndices

--- lib/Retazos.hs
+++ lib/Retazos.hs
@@ -1,10 +1,13 @@
+{-# LANGUAGE ImportQualifiedPost #-}
 module Retazos where
 
 import Prelude hiding (lines)
 import Data.ByteString
+import Data.ByteString qualified as B
 
 data Range = Range Int Int deriving Show
 
 lines :: ByteString -> [Range]
-lines = undefined
-
+lines bytes = 
+    let go index crs lfs = undefined
+     in go 0 (B.elemIndices 0x0A bytes) (B.elemIndices 0x0D bytes)
