fc23b85b16aa48d249247aca5f5690e6574316b7
Daniel Diaz <diaz.carrete@facebook.com>
2021-02-16 22:07:22 +0100

    lines impl

--- lib/Retazos.hs
+++ lib/Retazos.hs
@@ -1,13 +1,17 @@
 {-# LANGUAGE ImportQualifiedPost #-}
+
 module Retazos where
 
-import Prelude hiding (lines)
 import Data.ByteString
 import Data.ByteString qualified as B
+import Prelude hiding (lines)
 
-data Range = Range Int Int deriving Show
+data Range = Range Int Int deriving (Show)
 
 lines :: ByteString -> [Range]
-lines bytes = 
-    let go index crs lfs = undefined
-     in go 0 (B.elemIndices 0x0A bytes) (B.elemIndices 0x0D bytes)
+lines bytes =
+  let go index crs@(ci : crs') lfs@(li : lfs') = case compare (succ ci) li of
+        LT -> Range index (index - ci) : go (succ ci) crs' lfs
+        EQ -> Range index (index - ci) : go (succ li) crs' lfs'
+        GT -> Range index (index - li) : go (succ li) crs lfs'
+   in go 0 (B.elemIndices 0x0A bytes) (B.elemIndices 0x0D bytes)
